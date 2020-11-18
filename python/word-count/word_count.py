a66aaacd8fea05dfc1698df91129100c502d78fa
wchen928 <lchen.msc2019@ivey.ca>
2020-11-18 15:17:09 -0800

    revised

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,11 @@
+from collections import Counter
+import re
+
 def count_words(sentence):
-    pass
+    sentence = sentence.lower()
+
+    #split the sentences
+    words = re.findall("[0-9]+|[a-z]+['][a-z]|[a-z]+", sentence)
+
+    c = Counter(words)
+    return dict(c)
