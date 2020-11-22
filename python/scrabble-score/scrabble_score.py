58a41ee14f86cdac769ed453b808275925d8a3c0
wchen928 <lchen.msc2019@ivey.ca>
2020-11-21 16:09:25 -0800

    revision 1

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,23 +1,36 @@
 from collections import Counter
 
-value = {
-    'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 
-    'G': 2, 'H': 4, 'I': 1, 'J': 8, 'K': 5, 'L': 1,
-    'M': 3, 'N': 1, 'O': 1, 'P': 3, 'Q': 10, 'R': 1,
-    'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8,
-    'Y': 4, 'Z': 10
+values = {
+    1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+    2: ['D', 'G' ],
+    3: ['B', 'C', 'M', 'P'],
+    4: ['F', 'H', 'V', 'W', 'Y'],
+    5: ['K'],
+    8: ['J', 'X'],
+    10: ['Q', 'Z']
 }
+
+def get_value(d):
+    table = {}
+    for k, v in d.items():
+        for i in v:
+            table[i] = k
+    return table
+
+
 
 def score(word):
     word_c = Counter(word.upper())
-    add = [value[letter]*word_c[letter] for letter in word_c]
+    value_table = get_value(values)
+    add = [value_table[letter]*word_c[letter] for letter in word_c]
     return sum(add)
 
 
 
 def score_n_better(word):
     word = word.upper()
-    add = [value[c] for c in word]
+    value_table = get_value(values)
+    add = [value_table[c] for c in word]
     return sum(add)
 
 
@@ -41,15 +54,3 @@ def score_n(word):
         else:
             res += 5
     return res
-
-"""
-value = {
-    1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
-    2: ['D', 'G' ],
-    3: ['B', 'C', 'M', 'P'],
-    4: ['F', 'H', 'V', 'W', 'Y'],
-    5: ['K'],
-    8: ['J', 'X'],
-    10: ['Q', 'Z']
-}
-"""
