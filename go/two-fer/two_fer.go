2602e735205106eb4b576136c61ed58eb4f4a329
wchen928 <lchen.msc2019@ivey.ca>
2020-11-25 19:23:15 -0800

    w/out package

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -2,13 +2,10 @@
 // https://golang.org/doc/effective_go.html#commentary
 package twofer
 
-import "fmt"
-
 // ShareWith should have a comment documenting it.
 func ShareWith(name string) string {
 	if name == "" {
-		return fmt.Sprintf("One for you, one for me.")
-	} else {
-		return fmt.Sprintf("One for %v, one for me.", name)
+		name = "you"
 	}
+	return "One for " + name + ", one for me."
 }
