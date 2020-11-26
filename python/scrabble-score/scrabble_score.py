aa207badb6a9f9583b793bc95b0e2e3ccc29093f
wchen928 <lchen.msc2019@ivey.ca>
2020-11-25 19:39:06 -0800

    IntEnum method

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,56 +1,24 @@
 from collections import Counter
+from enum import IntEnum
 
-values = {
-    1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
-    2: ['D', 'G' ],
-    3: ['B', 'C', 'M', 'P'],
-    4: ['F', 'H', 'V', 'W', 'Y'],
-    5: ['K'],
-    8: ['J', 'X'],
-    10: ['Q', 'Z']
-}
 
-def get_value(d):
-    table = {}
-    for k, v in d.items():
-        for i in v:
-            table[i] = k
-    return table
-
+class Letters(IntEnum):
+    A = E = I = O = U = L = N = R = S = T = 1
+    D = G = 2
+    B = C = M = P = 3
+    F = H = V = W = Y = 4
+    K = 5
+    J = X = 8
+    Q = Z = 10
 
 
 def score(word):
     word_c = Counter(word.upper())
-    value_table = get_value(values)
-    add = [value_table[letter]*word_c[letter] for letter in word_c]
+    add = [Letters[letter]*word_c[letter] for letter in word_c]
     return sum(add)
 
 
-
-def score_n_better(word):
-    word = word.upper()
-    value_table = get_value(values)
-    add = [value_table[c] for c in word]
+def score_space(word):
+    word_c = word.upper()
+    add = [Letters[letter] for letter in word_c]
     return sum(add)
-
-
-
-def score_n(word):
-    word = word.upper()
-    res = 0
-    for c in word:
-        if c in ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T']:
-            res += 1
-        elif c in ['D', 'G' ]:
-            res += 2
-        elif c in ['B', 'C', 'M', 'P']:
-            res += 3
-        elif c in ['F', 'H', 'V', 'W', 'Y']:
-            res += 4
-        elif c in ['J', 'X']:
-            res += 8
-        elif c in ['Q', 'Z']:
-            res += 10
-        else:
-            res += 5
-    return res
