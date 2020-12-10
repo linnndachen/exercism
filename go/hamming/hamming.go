bc4ce0ac8c1c33d922b4737ac56e8a5420cecca9
wchen928 <lchen.msc2019@ivey.ca>
2020-12-10 13:44:08 -0800

    revision 2:lint

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -4,17 +4,16 @@ import "errors"
 
 //Distance is a function for counting how many different characters are there between 2 strings.
 func Distance(a, b string) (int, error) {
-	res := 0
 	runeA := []rune(a)
 	runeB := []rune(b)
+	res := 0
 	if len(runeA) != len(runeB) {
 		return res, errors.New("incorrect input")
 	}
 	for i := range runeA {
 		if runeA[i] != runeB[i] {
-			res ++
+			res++
 		}
 	}
 	return res, nil
 }
-
