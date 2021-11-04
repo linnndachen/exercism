c655358923fcb23071cfe9445360e3ca7d304447
wchen928 <lchen.msc2019@ivey.ca>
2021-11-04 07:39:48 -0700

    v2

--- java/two-fer/src/main/java/Twofer.java
+++ java/two-fer/src/main/java/Twofer.java
@@ -1,12 +1,6 @@
 public class Twofer {
     public String twofer(String name) {
-        String person;
-
-        if (name == null) {
-            person = "you";
-        } else {
-            person = name;
-        }
+        String person = (name == null) ? "you" : name;
 
         return "One for " + person + ", one for me.";
     }
