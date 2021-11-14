b5fe978f1799f66f0eaeeeec96560b760fb976ae
wchen928 <lchen.msc2019@ivey.ca>
2021-11-14 10:34:21 -0800

    v1

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,36 @@
+import java.lang.Math;
+
 public class CarsAssemble {
 
+    public double totalProduction(int speed) {
+        if (speed <= 4) {
+            return speed * 221;
+        } else if (speed <= 8) {
+            return speed * 221 * 0.9;
+        } else if (speed == 9) {
+            return speed * 221 * 0.8;
+        } else {
+            return speed * 221 * 0.77;
+        }
+    }
+
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the AssemblyLine.productionRateperHour() method");
+        return totalProduction(speed);
+
+        // throw new UnsupportedOperationException("Please implement the AssemblyLine.productionRateperHour() method");
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the AssemblyLine.workingItemsPerMinute() method");
+        return (int) (totalProduction(speed)/60.0);
+        // throw new UnsupportedOperationException("Please implement the AssemblyLine.workingItemsPerMinute() method");
     }
+
+    /*
+    public static void main(String[] args) {
+        CarsAssemble car = new CarsAssemble();
+        System.out.println(car.workingItemsPerMinute(10));
+        System.out.println(car.productionRatePerHour(10));
+
+    }
+     */
 }
