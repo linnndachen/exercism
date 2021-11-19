a22bd6f5a91326b29faf220b389da1577443ee1f
wchen928 <lchen.msc2019@ivey.ca>
2021-11-19 11:18:29 -0800

    v1

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,29 +1,32 @@
-import java.lang.*;
+import java.lang.StringBuilder;
 
 class SqueakyClean {
     static String clean(String identifier) {
         identifier = identifier.replaceAll(" ", "_");
         identifier = identifier.replaceAll("\\p{Cntrl}", "CTRL");
-        identifier = identifier.replaceAll("[α-ω]", "");
+        identifier = identifier.replaceAll("[α-ω0-9]", "");
 
+        StringBuilder string = new StringBuilder(identifier);
         int i = 0;
-        while(i < identifier.length()) {
-            Character letter = identifier.charAt(i);
+        while(i < identifier.length()-1) {
+            String letter = string.substring(i, i+1);
             if (letter.matches("-")) {
                 char nxt = identifier.charAt(i+1);
                 char newNxt = Character.toUpperCase(nxt);
-                identifier = identifier.replace(nxt, newNxt);
-                identifier = identifier.replace(letter, Character.MIN_VALUE);
+                string.setCharAt(i+1, newNxt);
+                string.deleteCharAt(i);
+                // identifier = identifier.replace(letter, Character.MIN_VALUE);
+            } else {
+                i++;
             }
 
-            i++;
         }
 
-        return identifier;
+        return string.toString();
     }
 
     public static void main(String[] args) {
         SqueakyClean test = new SqueakyClean();
-        System.out.println(test.clean("à-ḃç"));
+        System.out.println(test.clean("\uD83D\uDE00\uD83D\uDE00\uD83D\uDE00"));
     }
 }
