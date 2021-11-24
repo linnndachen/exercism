3f0c12253cf5c6e853772846af2c22457e0d8c1f
wchen928 <lchen.msc2019@ivey.ca>
2021-11-24 12:05:11 -0800

    v3

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -11,30 +11,23 @@ class SqueakyClean {
         String regex = "[^\\p{L}\\p{N}\\p{P}\\p{Z}]";
         identifier = identifier.replaceAll(regex, "");
 
-        // task 4: omit lower case Greek letter
+        // task 4: omit lower case Greek letter and numbers
         identifier = identifier.replaceAll("[α-ω0-9]", "");
 
         // task 3: Convert kebab-case to camelCase
-        StringBuilder string = new StringBuilder(identifier);
-        int i = 0;
-        while(i < identifier.length()-1) {
-            String letter = string.substring(i, i+1);
-            if (letter.matches("-")) {
-                char nxt = identifier.charAt(i+1);
-                char newNxt = Character.toUpperCase(nxt);
-                string.setCharAt(i+1, newNxt);
-                string.deleteCharAt(i);
-                // identifier = identifier.replace(letter, Character.MIN_VALUE);
-            } else {
-                i++;
-            }
-
+        if (identifier.contains("-")) {
+            StringBuilder string = new StringBuilder(identifier);
+            int idx = string.lastIndexOf("-");
+            string.replace(idx, idx + 2, string.substring(idx + 1, idx + 2).toUpperCase());
+            return string.toString();
+        } else {
+            return identifier;
         }
-        return string.toString();
+
     }
 
     public static void main(String[] args) {
         SqueakyClean test = new SqueakyClean();
-        System.out.println(test.clean("\uD83D\uDE00\uD83D\uDE00\uD83D\uDE00"));
+        System.out.println(test.clean("my   Id"));
     }
 }
