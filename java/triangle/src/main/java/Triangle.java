87631d9a813217a5585c3922ec4fc48282aa1763
wchen928 <lchen.msc2019@ivey.ca>
2021-11-29 20:31:48 -0800

    v3

--- java/triangle/src/main/java/Triangle.java
+++ java/triangle/src/main/java/Triangle.java
@@ -4,9 +4,7 @@ class Triangle {
     private final double side3;
 
     Triangle(double side1, double side2, double side3) throws TriangleException {
-        if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
-            throw new TriangleException();
-        }
+        if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) throw new TriangleException();
 
         this.side1 = side1;
         this.side2 = side2;
