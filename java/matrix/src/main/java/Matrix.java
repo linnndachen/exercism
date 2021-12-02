07064e9568a66326ab06b56d4bff167f68c75769
wchen928 <lchen.msc2019@ivey.ca>
2021-12-02 12:04:44 -0800

    v3

--- java/matrix/src/main/java/Matrix.java
+++ java/matrix/src/main/java/Matrix.java
@@ -5,39 +5,33 @@ import java.util.stream.Collectors;
 
 class Matrix {
     private final List<List<Integer>> matrix = new ArrayList<List<Integer>>();
-    private final int row;
 
     Matrix(String matrixAsString) {
         // turn string into a list of lists
+        // ["1 2 3", "4 5 6"]
         String lines[] = matrixAsString.split("\n");
-        for (String l : lines) {
-            List<Integer> arrList = Arrays.stream(l.split(" "))
-                                        .map(Integer::parseInt)
-                                        .collect(Collectors.toList());
-            matrix.add(arrList);
-        }
-        // set row and col
-        row = matrix.size();
+        List<Integer> arrList = Arrays.stream(lines) // "1 2 3" Stream<String>
+                // ["1", "2", "3"] want: Stream<List<String>> now: Stream<ArrayList<String>>
+                .map(l -> l.split(" "))
+                .flatMap(s->s.stream()) // Stream<String>
+                .mapToInt(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        matrix.add(arrList);
     }
 
     int[] getRow(int rowNumber) {
 
-        return matrix.get(rowNumber-1).stream().mapToInt(Integer::intValue).toArray();
+        return matrix.get(rowNumber-1).stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 
     int[] getColumn(int columnNumber) {
 
-        return matrix.stream().map(row->row.get(columnNumber-1)).mapToInt(Integer::intValue).toArray();
+        return matrix.stream()
+                .map(row->row.get(columnNumber-1))
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
-
-    // for testing
-    /*
-    public static void main(String[] args){
-        String matrixAsString = "1 2 3\n4 5 6\n7 8 9\n8 7 6";
-        Matrix test = new Matrix(matrixAsString);
-        System.out.println(test.getColumn(3));
-        System.out.println(test.getRow(3));
-    }
-
-     */
 }
