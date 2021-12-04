9e85949ad4816843dc7e1d9f71efc795e6c4acfe
wchen928 <lchen.msc2019@ivey.ca>
2021-12-03 16:53:08 -0800

    v1

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -1,10 +1,25 @@
-/*
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
 
-Please remove this comment when submitting your solution.
+class WordCount {
 
-*/
+    Map<String, Integer> phrase(String sentence) {
+        Map<String, Integer> wordCount = new HashMap<String, Integer>();
+        sentence = sentence.replaceAll("[^A-Za-z0-9' ]+|(?<=^|\\W)'|'(?=\\W|$)", " ")
+                .trim()
+                .toLowerCase();
+
+        String[] words = sentence.split("\\s+");
+
+        for (int i = 0; i < words.length; i++) {
+            String key = words[i];
+            int freq = wordCount.getOrDefault(key, 0);
+            wordCount.put(key, ++freq);
+        }
+
+        return wordCount;
+    }
+
+}
